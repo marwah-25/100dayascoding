fe83e1e09182f8a0a34622ecc16d3eec94801e99
marwah-25 <wawamarwah660@gmail.com>
2026-09-02 19:30:12 +0700

    Create day01.java

---
+++ day01.java
@@ -0,0 +1,20 @@
+/**
+ * Jenis 3: Komentar JavaDoc (Dokumentasi)
+ * @author Marwah
+ */
+public class day01 { // STRUKTUR DASAR 1: Public Class
+
+    public static void main(String[] args) { // STRUKTUR DASAR 2: Main Method
+        
+        // Jenis 1: Komentar Satu Baris
+        System.out.println("Nama saya Marwah");
+
+        /*
+          Jenis 2: Komentar Banyak Baris
+          Perintah di bawah ini digunakan untuk 
+          menampilkan teks ke layar konsol.
+        */
+        System.out.println("commit hari pertama");
+        
+    }
+}
