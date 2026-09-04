e179dee80d3c197364f882829c93fff17a1be568
marwah-25 <wawamarwah660@gmail.com>
2026-09-04 14:57:55 +0800

    Create day3.java

---
+++ day3.java
@@ -0,0 +1,30 @@
+public class day3 {
+    public static void main(String[] args) {
+
+        // 1. Judul (menggunakan \t agar tulisan geser ke tengah)
+        System.out.println("==================================");
+        System.out.println("\tPROFIL MAHASISWA");
+        System.out.println("==================================\n"); // \n untuk enter / baris kosong
+
+        // 2. Isi Biodata (kombinasi print dan println agar menyambung)
+        System.out.print("Nama\t: ");
+        System.out.println("Marwah");
+
+        System.out.print("NIM\t: ");
+        System.out.println("D0224303");
+
+        System.out.print("Prodi\t: ");
+        System.out.println("Informatika");
+
+        // 3. Motto (menggunakan \" untuk menampilkan tanda kutip)
+        System.out.print("kata2\t: ");
+        System.out.println("\"Saya pasti bisa selesaikan day3Coding\"");
+
+        // 4. Folder (menggunakan \\ untuk menampilkan backslash)
+        System.out.print("Folder\t: ");
+        System.out.println("C:\\JAVA\\100DaysCoding\n");
+
+        System.out.println("==================================");
+    }
+}
+
