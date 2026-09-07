218ec58abce68bb2f910fa787fcf1b26f6df2d15
marwah-25 <wawamarwah660@gmail.com>
2026-09-07 18:44:29 +0700

    Update day6.java

--- day6.java
+++ day6.java
@@ -1,24 +1,21 @@
 public class day6 {
     public static void main(String[] args) {
         
-        // char ialah tipe data primitif yang hanya menampung 1 karakter/simbol/nilai, entahkah itu 1,A,@ dan harus menggunakan petik 1 ('')
+        // char ialah tipe data primitif yang hanya menampung 1 karakter/simbol/nilai, entahkah itu 1,A,@ dan wajib menggunakan petik 1 ('')
         char a = 'A';
 
         // boolean ialah tioe data peimitif yang hanya menghasilkan true atau false
         boolean b = true;
 
-        // Teks Tambahan (Nama)
-        String c = "Aldy";
-
-        // Menampilkan hasil yang dimau
+        // Menampilkan hasil yang di mau
         System.out.println("============================");
         System.out.println("\tDATA MAHASISWA");
         System.out.println("============================");
-        System.out.println("");
-        System.out.println("Nama : " + a);
-        System.out.println("Grade : " + b);
-        System.out.println("Aktif : " + c);
-        System.out.println("");
+        System.out.println(" ");
+        System.out.println("Nama : Aldy");
+        System.out.println("Grade : " + a);
+        System.out.println("Aktif : " + b);
+        System.out.println(" ");
         System.out.println("============================");
     }
 }
