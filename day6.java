371a2ba9747deba17d64697c357fa53b9d7ec906
marwah-25 <wawamarwah660@gmail.com>
2026-09-07 18:35:42 +0700

    Create day6.java

---
+++ day6.java
@@ -0,0 +1,24 @@
+public class day6 {
+    public static void main(String[] args) {
+        
+        // char ialah tipe data primitif yang hanya menampung 1 karakter/simbol/nilai, entahkah itu 1,A,@ dan harus menggunakan petik 1 ('')
+        char a = 'A';
+
+        // boolean ialah tioe data peimitif yang hanya menghasilkan true atau false
+        boolean b = true;
+
+        // Teks Tambahan (Nama)
+        String c = "Aldy";
+
+        // Menampilkan hasil yang dimau
+        System.out.println("============================");
+        System.out.println("\tDATA MAHASISWA");
+        System.out.println("============================");
+        System.out.println("");
+        System.out.println("Nama : " + a);
+        System.out.println("Grade : " + b);
+        System.out.println("Aktif : " + c);
+        System.out.println("");
+        System.out.println("============================");
+    }
+}
