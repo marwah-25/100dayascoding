ff0cd6fec8a2c25409b4723454dc60a6429f7548
marwah-25 <wawamarwah660@gmail.com>
2026-09-08 16:15:59 +0700

    Create day7.java

---
+++ day7.java
@@ -0,0 +1,25 @@
+public class day7 {
+    public static void main(String[] args) {
+        
+        // String: tipe data ajaib atau Non primitif yang gunanya untuk menyimpan teks atau dia hanya mencetak teks (wajib diapit petik ganda " ")
+        String a = "Aldy";
+        String b = "12345678";
+        String c = "TI-4A";
+        String d = "Teknik Informatika";
+        String e = "Coding";
+
+        // Menampilkan output yang di pengen
+        System.out.println("=========================");
+        System.out.println("    BIODATA MAHASISWA");
+        System.out.println("=========================");
+        System.out.println(" ");
+        System.out.println("Nama     : " + a);
+        System.out.println("NIM      : " + b);
+        System.out.println("Kelas    : " + c);
+        System.out.println("Jurusan  : " + d);
+        System.out.println("Hobi     : " + e);
+        System.out.println(" ");
+        System.out.println("=========================");
+    }
+}
+
