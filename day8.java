eab60c98d92e01614e6323865a9825b0d886a2d9
marwah-25 <wawamarwah660@gmail.com>
2026-09-09 14:37:55 +0700

    Create day8.java

---
+++ day8.java
@@ -0,0 +1,26 @@
+public class day8 {
+    public static void main(String[] args) {
+        // 1. Deklarasi
+        String nama;
+        int umur;
+        double tb;
+
+        // 2. Inisialisasi
+        nama = "marwah";
+        umur = 20;
+        tb = 154.5;
+
+        // Tampilkan nilai sebelum di-update
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur sebelum di update: " + umur);
+        System.out.println("Tinggi: " + tb);
+
+        // 3. Update
+        umur = 21;
+
+        // menampilkan nilai sudah di-update
+        System.out.println("Umur setelah diupdate: " + umur);
+    
+
+    }
+}
