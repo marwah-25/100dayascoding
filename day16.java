573a713d65f1d8333922d9c06f5e634c38eb0511
marwah-25 <wawamarwah660@gmail.com>
2026-09-17 11:02:55 +0700

    Create day16.java

---
+++ day16.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class day16 {
+
+    public static void main(String[] args) {
+        Scanner oh = new Scanner(System.in);
+
+        System.out.println("Input");
+        System.out.print("Masukkan jumlah permen: ");
+        int a = oh.nextInt();
+
+        System.out.print("Masukkan jumlah siswa: ");
+        int b = oh.nextInt();
+
+        int jumlahSetiapSiswa = a/b;
+        int sisa = a%b;
+
+        System.out.println();
+        System.out.println("Output");
+        System.out.println("Jumlah permen\t: "+a);
+        System.out.println("Jumlah siswa\t: "+ b);
+        System.out.println("Setiap siswa mendapatkan : "+jumlahSetiapSiswa+" permen");
+        System.out.println("Permen yang tersisa\t: "+sisa+" permen");
+    }
+}
