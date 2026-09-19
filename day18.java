de5d3926d9dba572393c96ffce37ffbcdd635e5d
marwah-25 <wawamarwah660@gmail.com>
2026-09-19 15:17:32 +0700

    Create day18.java

---
+++ day18.java
@@ -0,0 +1,18 @@
+public class day18 {
+    public static void main(String[] args) {
+        byte nilaiByte = 27;
+        short nilaiShort = nilaiByte;
+        int nilaiInt = nilaiShort;
+        long nilaiLong = nilaiInt;
+        float nilaiFloat = nilaiLong;
+        double nilaiDouble = nilaiFloat;
+
+        System.out.println("NIlai byte\t: "+nilaiByte);
+        System.out.println("Nilai short\t: "+nilaiShort);
+        System.out.println("Nilai int\t: "+nilaiInt);
+        System.out.println("Nilai long\t: "+nilaiLong);
+        System.out.println("Nilai float\t: "+nilaiFloat);
+        System.out.println("Nilai double\t: "+nilaiDouble);
+    }
+    
+}
