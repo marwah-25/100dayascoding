57b823d7a0f6999c2ab2a630a7c9d445375adbce
marwah-25 <wawamarwah660@gmail.com>
2026-09-21 18:36:25 +0700

    Create day20.java

---
+++ day20.java
@@ -0,0 +1,22 @@
+
+public class day20 {
+    public static void main(String[] args) {
+        int umur = 20;
+        String a = String.valueOf(umur);
+
+        double tb = 155.0;
+        String b = String.valueOf(tb);
+
+        char KLS = 'C';
+        String c = String.valueOf(KLS);
+
+        boolean status = true;
+        String d = String.valueOf(status);
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}
